529612e5c58ed9106b11b978679529ab49e21fa8
sunglassguy <jason.gunther@gmail.com>
2026-02-04 11:40:28 -0500

    Update wyze_cloud_models.py
    Signed-off-by: sunglassguy <jason.gunther@gmail.com>

--- custom_components/wyzeapi/wyze_cloud_models.py
+++ custom_components/wyzeapi/wyze_cloud_models.py
@@ -1,1 +1,18 @@
+from __future__ import annotations
+
+import uuid
+from dataclasses import dataclass
+from typing import Optional, Any
+
+
+@dataclass
+class WyzeCredential:
+    access_token: Optional[str] = None
+    refresh_token: Optional[str] = None
+    user_id: Optional[str] = None
+    mfa_options: Optional[list] = None
+    mfa_details: Optional[dict[str, Any]] = None
+    sms_session_id: Optional[str] = None
+    email_session_id: Optional[str] = None
+    phone_id: str = str(uuid.uuid4())
 
